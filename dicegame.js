74d056a14c1140ddbe6e66b735d4e726fd0887eb
Adam <argrgic@gmail.com>
2019-02-19 16:17:54 -0600

    third commit

--- dicegame.js
+++ dicegame.js
@@ -7,15 +7,28 @@
 // As your sanity lowers, the complexity of dice increases
 
 
-let characterSanity;
 
-let characterInfo =[];
-let playerPoints;
-function rollDice() {
-	let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+
+// global variables -- these variables are inluded in more than one function
+let totalPoints;
+let randomNumber;
+let devilsPoints;
+let pointsAdded;
+
+// Linked to first submit button
+// Generates character information
+let createCharacter = function() {
+	characterInfo.name = document.getElementById("characterName").value;
+	characterInfo.city = document.getElementById("characterCity").value;
+	
+	console.log(characterInfo);
+	
 }
-
-function compareValues() {};
+// Character object 
+let characterInfo = {
+    sanity :100,
+	score : 0
+};
 
 let dice = {
 	dice1: {
@@ -67,60 +80,113 @@ let dice = {
 };
 
 
-// Linked to first submit button
-// Generates character information
-let createCharacter = function() {
-	characterInfo.push(document.getElementById("characterName").value);
-	characterInfo.push(document.getElementById("characterCity").value);
-	characterInfo.push(characterSanity = 100);
-	console.log(characterInfo);
-	
+
+// Function that does the actual dice roll
+function rollDice() {
+	randomNumber = Math.floor(Math.random() * dice.dice1.sides) + 1;
+	console.log(randomNumber);
+	return randomNumber;
 }
 
+
+
+//User rolls dice three times for points
 function userRollDice() {
-	if (characterInfo[2] >= 85) {
-		thing(dice.dice1);
-		}
-		
+	let roll1 = rollDice();
+	let roll2 = rollDice();
+	let roll3 = rollDice();
+	totalPoints = roll1 + roll2 + roll3;
+	console.log("Roll 1: " + roll1 + " Roll 2: " + roll2 + " Roll 3: " + roll3 + " Total Score: " + totalPoints);
+	return totalPoints; 
+}
 	
-	else if (85 > characterInfo[2] >= 70) {
-		thing(dice.dice2);
-		}
-		
 	
-	else if (70 > characterInfo[2] >= 55) {
-		thing(dice.dice3);
-		}
-		
+// Function that adds points to user score
+function addScore() {
+	characterInfo.score += totalPoints;
+	console.log(totalPoints + " points were added to your score");
+	console.log(characterInfo.score);
+	return characterInfo.score;
 	
-	else if (55 > characterInfo[2] >= 40) {
-		thing(dice.dice4);
-		}
-		
+}
 	
-	else if (40 > characterInfo[2] >= 25) {
-		thing(dice.dice5);
-		
+
+// Function that lets the devil roll the dice
+function devilRoll() {
+	let devilRoll1 = rollDice();
+	let devilRoll2 = rollDice();
+	let devilRoll3 = rollDice();
+	devilsPoints = devilRoll1 + devilRoll2 + devilRoll3;
+	console.log("the devil rolled " + devilsPoints);
+}
+
+// Function that subtracts the devilsPoints from the usersPoints
+function modifyTotals() {
+	pointsAdded = totalPoints - devilsPoints;
+	characterInfo.score += pointsAdded;
+	console.log(pointsAdded + " points were added to your score.");
+}
+
+// Function that implements all other dice rolling related events
+function playGame() {
+	userRollDice();
+	addScore();
+	devilRoll();
+	modifyTotals();
+	if (pointsAdded % 6 === 0) {
+		characterInfo.sanity -= pointsAdded;
+		console.log(characterInfo.sanity);
 	}
-	else if (25 > characterInfo[2] >= 10) {
-		thing(dice.dice6);
-		
-	}
-	else {
-			alert("Your sanity is too low. You lose the game.")
-		}
-	}
-	
-	
 	
 }
 
 
-function thing(diceToRoll){
-	diceToRoll.roll;
-		if (randomNumber = 6) {
-			characterInfo[2] -= 15;
-}
+
+//	
+//function 
+//	if (characterInfo[2] >= 85) {
+//		thing(dice.dice1);
+//		}
+//		
+//	
+//	else if (85 > characterInfo[2] >= 70) {
+//		thing(dice.dice2);
+//		}
+//		
+//	
+//	else if (70 > characterInfo[2] >= 55) {
+//		thing(dice.dice3);
+//		}
+//		
+//	
+//	else if (55 > characterInfo[2] >= 40) {
+//		thing(dice.dice4);
+//		}
+//		
+//	
+//	else if (40 > characterInfo[2] >= 25) {
+//		thing(dice.dice5);
+//		
+//	}
+//	else if (25 > characterInfo[2] >= 10) {
+//		thing(dice.dice6);
+//		
+//	}
+//	else {
+//			alert("Your sanity is too low. You lose the game.")
+//		}
+//	}
+//	
+//	
+//	
+//}
+//
+//
+//function thing(diceToRoll){
+//	diceToRoll.roll;
+//		if (randomNumber = 6) {
+//			characterInfo[2] -= 15;
+//}
 
 
 
