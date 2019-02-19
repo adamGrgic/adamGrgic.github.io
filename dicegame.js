b14656590d73034a1abda54c6876203eef171753
Adam <argrgic@gmail.com>
2019-02-18 21:32:26 -0600

    first commit

---
+++ dicegame.js
@@ -0,0 +1,139 @@
+// JavaScript Document
+
+// Welcome the user to the game
+
+
+
+// As your sanity lowers, the complexity of dice increases
+
+
+let characterSanity = 0;
+
+let characterDetails = new Object();
+
+
+let dice = {
+	
+};
+
+
+let characterInfo = { 
+	name: document.getElementById('characterName').value;
+	city: document.getElementById('characterCity').value;
+	characterSanity === 100;
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    These are lines of code from a previous version -- I hated it and started over :) //
+
+
+
+
+
+
+
+// dice operations
+
+
+//var dice = {
+//  sides: 6,
+//  roll: function () {
+//    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//    return randomNumber;
+//  }
+//}
+//
+//var dice2 = {
+//  sides: 8,
+//  roll: function () {
+//    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//    return randomNumber;
+//  }
+//}
+//
+//var dice3 = {
+//  sides: 10,
+//  roll: function () {
+//    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//    return randomNumber;
+//  }
+//}
+//
+//var dice4 = {
+//  sides: 12,
+//  roll: function () {
+//    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//    return randomNumber;
+//  }
+//}
+//
+//var dice5 = {
+//  sides: 16,
+//  roll: function () {
+//    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//    return randomNumber;
+//  }
+//}
+//
+//var dice6 = {
+//  sides: 26,
+//  roll: function () {
+//    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//    return randomNumber;
+//  }
+//}
+
+
+
+
+//Prints dice roll to the page
+//
+//function printNumber(number) {
+//  var placeholder = document.getElementById('placeholder');
+//  placeholder.innerHTML = number;
+//}
+//
+//var button = document.getElementById('button');
+//
+//button.onclick = function() {
+//  var result = dice.roll();
+//  printNumber(result);
+//};
+//
+//
+//
+//let character = document.getElementById('characterInfo')= {
+//	let characterName = characterinfo.name;
+//	let characterCity = characterinfo.city;
+//	
+//};
+
+// User rolls
+
+// Dice is rolled and a number is chosen between 1 and 6 
+
+// Number is generated / stored within the system
+// Number is reported to the user 
+
+
+
